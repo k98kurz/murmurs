335e31505705e8640f476632dc174a425244e705
Jon Tyler <k98kurz@gmail.com>
2023-06-20 17:26:59 -0400

    removed extraneous interfaces

--- murmurs/interfaces.py
+++ murmurs/interfaces.py
@@ -1,68 +1,25 @@
+from __future__ import annotations
 from typing import Any, Protocol, runtime_checkable
 
 
 @runtime_checkable
 class CanJsonSerialize(Protocol):
+    """Interface for classes that can serialize to and from json."""
     def to_json(self) -> str:
+        """Return serialized instance data json."""
         ...
 
     @classmethod
-    def from_json(cls, data: str) -> None:
+    def from_json(cls, data: str) -> CanJsonSerialize:
+        """Deserialize data from json and return instance."""
         ...
 
 
 @runtime_checkable
-class CanSendMessages(Protocol):
-    def send(self, message: bytes, to: bytes) -> None:
-        ...
-
-
-@runtime_checkable
-class CanRouteMessages(Protocol):
-    def route(self, message: bytes, to: bytes) -> None:
-        ...
-
-
-@runtime_checkable
-class CanElectRoot(Protocol):
-    def current_root(self) -> Any:
-        ...
-
-    def check_new_root(self, condition: Any) -> bool:
-        ...
-
-    def elect_new_root(self, new_root: Any) -> bytes:
-        ...
-
-
-@runtime_checkable
-class CanCreateSpanningTree(Protocol):
-    def has_parent(self) -> bool:
-        ...
-
-    def check_new_parent(self, parent: Any) -> bool:
-        ...
-
-    def set_parent(self, parent: Any) -> None:
-        ...
-
-    def has_children(self) -> bool:
-        ...
-
-    def check_new_Child(self, child: Any) -> bool:
-        ...
-
-    def add_child(self, child: Any) -> bool:
-        ...
-
-
-@runtime_checkable
-class CanCalculateAddress(Protocol):
-    def calculate_address(self, coordinates: list[Any]) -> bytes:
-        ...
-
-
-@runtime_checkable
-class CanAssignAddress(Protocol):
-    def assign_address(self, target: Any, address: bytes) -> bytes:
+class CanUnicast(Protocol):
+    """Interface for classes that can handle unicast message sending."""
+    def unicast(self, message: Any, dst: Any) -> bool:
+        """Returns True if the message can be sent to the dst and False
+            otherwise.
+        """
         ...
